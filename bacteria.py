08dbe2b78116a8c07be0b809b6b74bbacf7540f3
jecalles <callesjonathan@gmail.com>
2017-11-27 12:32:35 -0800

    welcome to the thunderdome

---
+++ bacteria.py
@@ -0,0 +1,45 @@
+#import dependencies
+import numpy as np
+import matplotlib.pyplot as plt
+from uuid import uuid4 as uuid
+from codonUtils import utils
+
+class strain():
+    '''A class used to represent a bacterial strain in culture'''
+    def __init__(self, N_pop=100, k_grow=0.05, k_death=0.04, mu=1e-3,
+        codonTable=None, ID=None, lineage=[]):
+        '''The init function for the strain class.
+
+        Parameters
+        ----------
+        - int/float N_pop: current population size of strain
+        - float k_grow: growth rate of bacterial strain (1/min)
+        - float k_death: death rate of bacterial strain (1/min)
+        - float mu: mutation rate of bacterial strain (1/genome-gen)
+        - dict codonTable: genetic code for bacterial strain
+        - str ID: a unique string identifier for bacterial strain
+        - list <str> lineage: a list tracking the lineage of bacterial strain
+
+        Returns
+        -------
+        strain obj: returns handle for strain instance
+        '''
+        # generate values for optional parameters
+        if codonTable == None:
+            codonTable = utils.standardTable
+        if ID == None:
+            ID = uuid()
+        # store parameters in class attributes
+        self.N_pop = N_pop
+        self.k_grow = k_grow
+        self.k_death = k_death
+        self.mu = mu
+        self.codonTable = codonTable
+        self.ID = ID
+        self.lineage = lineage
+
+# run as script
+if __name__ == '__main__':
+    print(help(strain))
+    test_strain = strain()
+    print('all good!')
---
+++ thunderflask.py
@@ -0,0 +1,18 @@
+#import dependencies
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from codonTable import codonTable
+from codonUtils import utils
+
+# define simulator class
+class thunderflask():
+    '''Welcome to the Thunderdome of E. coli. A class used to simulate the
+    genetic diversity of a batch culture of e. coli given a genetic code'''
+
+    def __init__():
+        '''
+        Initializer for thunderflask class. Accepts a single strain or a list
+        of strains as an input argument
+        '''
+        return
