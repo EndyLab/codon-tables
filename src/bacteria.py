35c4a87f9ffe79dee3d17eaa9a87ca9647ea8d1f
Jon <jecalles@stanford.edu>
2018-02-28 12:12:00 -0800

    cleans up the filestructure

---
+++ src/bacteria.py
@@ -0,0 +1,97 @@
+#import dependencies
+import numpy as np
+import matplotlib.pyplot as plt
+from uuid import uuid4 as uuid
+from codonUtils import utils
+from codonTable import codonTable
+
+class strain():
+    '''A class used to represent a bacterial strain in culture'''
+    def __init__(self, N_pop=100, fitness=0, mu=2e-5, t_0=0,
+        t_est=0, table=None, ID=None, lineage=[]):
+        '''The init function for the strain class.
+
+        Parameters
+        ----------
+        - int/float N_pop: current population size of strain
+        - float fitness: the absolute fitness of an individual strain
+        - float mu: mutation rate of bacterial strain (1/genome-gen) assuming
+            Standard Code (automatically adjusts based on genetic code)
+        - float t_0: timestep in which a strain first arrives
+        - float t_est: timestep in which a strain becomes large enough to
+            simulate analytically (established)
+        - dict table: genetic code for bacterial strain
+        - str ID: a unique string identifier for bacterial strain
+        - list <str> lineage: a list tracking the lineage of bacterial strain
+
+        Returns
+        -------
+        strain obj: returns handle for strain instance
+        '''
+        # generate values for optional parameters
+        if table == None:
+            table = codonTable(utils.standardTable)
+        elif type(table) == dict:
+            table = codonTable(table=table)
+
+        if ID == None:
+            ID = str(uuid())
+        # update lineage to include own ID
+        lineage.append(ID)
+        # store parameters in class attributes
+        self.N_pop = N_pop
+        self.fitness = fitness
+        self.t_0 = t_0
+        self.t_est = t_est
+        self.table = table
+        self.mu = self.__getMutRate(mu)
+        self.ID = str(ID)
+        self.lineage = lineage
+        self.timepoints = []
+        self.poptrace = []
+
+    def __getMutRate(self, mu):
+        '''A private method used to calculate what the mutation rate should be for a particular strain given the rate for cells with the Standard Code.
+
+        Parameters
+        ----------
+        float mu: the mutation rate of this strain given the Standard Code
+
+        Returns
+        -------
+        float mu_adj: the adjusted mutation rate given the gentic code of the
+            strain
+        '''
+        # get dict form of codon table and of standard code
+        table = self.table.codonDict
+        sc = utils.standardTable
+        # if table is identical to the standard table, return mu
+        if table == sc:
+            mu_adj = mu
+        # if not, calculate how many substitutions are available to each code
+        else:
+            # initialize counters for the standard code and strain code
+            count_sc = 0
+            count = 0
+            # for each codon pair, one mutation separated
+            for (c1, c2) in utils.tripletMutPairs:
+                # increment counters if substitution is nonsynonymous AND
+                # neither codon encodes for a STOP
+                count_sc += not (
+                    (sc[c1] == '*' or sc[c2] == '*') or
+                    (sc[c1] == sc[c2]))
+                count += not (
+                    (table[c1] == '*' or table[c2] == '*') or
+                    (table[c1] == table[c2]))
+            # adjust mutation rate by ratio of substitutions
+            mu_adj = mu*(count/count_sc)
+
+        # return the adjusted mutation rate
+        return mu_adj
+
+# run as script
+if __name__ == '__main__':
+    import ipdb; ipdb.set_trace()
+    x = codonTable()
+    y = strain(table=x)
+    print('done')
---
+++ src/ffgen.py
@@ -0,0 +1,111 @@
+# import necessary modules
+import numpy as np
+import pandas as pd
+from codonUtils import utils
+from codonTable import codonTable
+from queue import Queue as queue
+from random import choice
+
+class ffgen():
+    ''' A class used to generate fast fail codon tables
+    '''
+
+    def __init__(self):
+        return
+
+    @staticmethod
+    def triplet():
+        '''
+        A function used to generate triplet decoding, fast fail genetic codes
+        using a rational, 'top down' approach. Snakes along codon table to fill
+        16 maximally distant codons first (all greater than one mutation from
+        each other), then randomly chooses 4 codons to place the remaining
+        amino acids'''
+
+        ############################
+        # fill out first 16 codons #
+        ############################
+
+        # declare caching variables
+        usedCodons = set()
+        unusedAA = set(utils.residues[:-1])
+        # declare budding table
+        table = {}
+        # randomly permute rNTPs for positions 1 and 2, store 3 unpermuted
+        pos1 = np.random.permutation(utils.rNTPs)
+        pos2 = np.random.permutation(utils.rNTPs)
+        pos3 = utils.rNTPs
+        # store nucleotides for positions 1 and 2 in queues
+        queue1 = queue()
+        queue2 = queue()
+        for nt1, nt2 in zip(pos1, pos2):
+            queue1.put(nt1)
+            queue2.put(nt2)
+
+        # place the first 16 elements
+        for i in range(queue1.qsize()):
+            # get first nucleotide of next codon
+            nt1 = queue1.get()
+            for j in range(queue2.qsize()):
+                # get second nucleotide of next codon
+                nt2 = queue2.get()
+                # get third nucleotide
+                nt3 = pos3[j]
+                # build codon, assign it a residue
+                codon = nt1 + nt2 + nt3
+                AA = choice(tuple(unusedAA))
+                table[codon] = AA
+                # update caching variables
+                usedCodons.add(codon)
+                unusedAA.remove(AA)
+                # re-enqueue second nucleotide
+                queue2.put(nt2)
+            # re-enqueue first nucleotide
+            queue1.put(nt1)
+
+        # place the last 4 elements at least 2 substitutions away
+        availableCodons = set(utils.tripletCodons) - usedCodons
+        for i in range(len(unusedAA)):
+            # pick a codon from the available set and assign it an amino acid
+            codon = choice(tuple(availableCodons))
+            AA = choice(tuple(unusedAA))
+            table[codon] = AA
+            # update cache variables
+            availableCodons = ffgen.updateAvailable3(codon, availableCodons)
+            usedCodons.add(codon)
+            unusedAA.remove(AA)
+
+        # assign unused codons to STOP
+        remainingCodons = set(utils.tripletCodons) - usedCodons
+        for codon in remainingCodons:
+            table[codon] = '*'
+
+        # return built table
+        return table
+
+    @staticmethod
+    def updateAvailable3(newCodon, availableSet):
+        ''' A static method used to update the set of codons that can be used
+        for triplet decoding fast fail code, given that a new codon is
+        occupied.''' 
+
+        # iterate over remaining codons
+        copySet = list(availableSet)
+        for codon in copySet:
+            # remove codons that have two nucleotide overlaps
+            count = 0
+            for i in range(len(newCodon)):
+                count += (codon[i] == newCodon[i])
+            if count >= 2:
+                availableSet.remove(codon)
+        # return updated set
+        return availableSet
+
+if __name__ == '__main__':
+    # availableCodons = set(utils.tripletCodons)
+    # count = 0
+    # while len(availableCodons) > 0:
+    #     codon = random.choice(tuple(availableCodons))
+    #     availableCodons = ffgen.updateAvailable3(codon, availableCodons)
+    #     count += 1
+    print(ffgen.triplet())
