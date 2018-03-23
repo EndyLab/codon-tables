b3944e396248c01dae24074681c3a2c25b0471ef
Jon <jecalles@stanford.edu>
2018-03-23 13:03:05 -0700

    makes test.py make pretty population traces

--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pl
-# import seaborn as sns
+from random import shuffle
+import seaborn as sns
 from src.thunderflask import thunderflask
 from src.bacteria import strain
 from tqdm import tqdm
@@ -13,24 +14,26 @@ sim = thunderflask(LUCA)
 # initialize some variables
 T_curr = 0
 mut_param = [1, 2]
-dt = 0.3
+dt = 0.1
+T_sim = 1000
 
 # run simulation
-sim.simulate(1000, dt, T_curr, mut_param, save_all=True, prune_strains=True,
+sim.simulate(T_sim, dt, T_curr, mut_param, save_all=True, prune_strains=True,
              show_progress=True)
-t = np.array(sim.f_avgtrace['timepoints'])
-f = np.array(sim.f_avgtrace['f_avg'])
-fig, axarr = plt.subplots(2, sharex=True)
-axarr[0].plot(t, f)
-axarr[0].set_title('Standard Code: Mean Fitness vs Time')
-dt = np.diff(t)
-t_avg = (t[1:]+t[:-1])/2
-gradf = np.diff(f)/dt
-axarr[1].plot(t_avg, gradf)
-axarr[1].set_title(r'Standard Code: $\frac{df}{dt}$ vs Time')
-plt.xlabel('Time (in generations)')
-plt.ylabel('Fitness (%)')
-plt.show()
+## plot results
+# t = np.array(sim.f_avgtrace['timepoints'])
+# f = np.array(sim.f_avgtrace['f_avg'])
+# fig, axarr = plt.subplots(2, sharex=True)
+# axarr[0].plot(t, f)
+# axarr[0].set_title('Standard Code: Mean Fitness vs Time')
+# dt = np.diff(t)
+# t_avg = (t[1:]+t[:-1])/2
+# gradf = np.diff(f)/dt
+# axarr[1].plot(t_avg, gradf)
+# axarr[1].set_title(r'Standard Code: $\frac{df}{dt}$ vs Time')
+# plt.xlabel('Time (in generations)')
+# plt.ylabel('Fitness (%)')
+# plt.show()
 
 # t = []
 # f = []
@@ -47,15 +50,21 @@ plt.show()
 # plt.show()
 
 n = len(sim.allStrains)
-colors = pl.cm.viridis(np.linspace(0,1,n))
-for i, bact in enumerate(sim.allStrains):
-    t = bact.timepoints
-    pop = bact.poptrace
-    plt.semilogy(t, pop, color=colors[i])
+colors = pl.cm.Greys(np.linspace(1,0.5,n))
+strainlist = [(i, bact) for i, bact in enumerate(reversed(sim.allStrains))]
+shuffle(strainlist)
+with sns.axes_style("white"):
+    for i, bact in strainlist:
+        t = bact.timepoints
+        pop = bact.poptrace
+        plt.semilogy(t, pop, color=colors[i])
 # for i, (key, (times, pops)) in enumerate(sim.poptrace.items()):
 #     if i % 10  == 0:
 #         plt.semilogy(times, pops, color=colors[i])
 plt.xlabel('Time (in generations)')
 plt.ylabel('Population Size')
 plt.title('Standard Code: Population Traces for Established Strains')
+plt.xlim([T_curr, T_sim])
+plt.ylim([1e0, 10**(6.2)])
+plt.savefig('/home/jonathan/Lab/Fast Fail/Figures/Figure 3/3a_strain_traces_2.svg')
 plt.show()
