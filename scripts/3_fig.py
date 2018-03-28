560620e22e6e992e3c5853a4a064997d858c8cee
Jon <jecalles@stanford.edu>
2018-03-28 16:18:07 -0700

    updates 3_fig to look at fast failing codes

--- scripts/3_fig.py
+++ scripts/3_fig.py
@@ -56,7 +56,7 @@ for s3_filename in pbar:
     pbar.set_description('Saving {0}'.format(s3_filename))
     basepath = os.path.basename(s3_filename)
     local_filename = 'res/{0}'.format(basepath)
-    s3.download_file(bucketname, s3_filename, local_filename)
+    # s3.download_file(bucketname, s3_filename, local_filename)
     local_filenames.append(local_filename)
 logging.info("Download Successful!")
 
@@ -72,15 +72,21 @@ logging.info("Concatenating Dataframes")
 DF = pd.concat(dfs, copy=False)
 
 # extract dataframe for figure 3
-wanted_codes = ['Colorado', 'Standard Code', 'FF20']
-f = lambda code: code in wanted_codes
+codes_3b = ['Colorado', 'Standard Code']
+codes_3c = list(set(DF['code']) - set(codes_3b))
+f = lambda code: code in codes_3b
+g = lambda code: not f(code)
 DF_3b = DF.loc[DF['code'].map(f)]
+DF_3c = DF.loc[DF['code'].map(g)]
 
 logging.info("Plotting 3B: Mean Fitness Traces")
 colordict = {
     'Standard Code' : 'blue',
     'Colorado' : 'red',
     'FF20' : 'green'
+    'FF16' : 'orange',
+    'Reductionist20' : 'purple'
+    'Reductionist15' : 'brown'
 }
 plt.figure()
 ax1 = sns.tsplot(
@@ -117,71 +123,38 @@ success_string = (
 )
 logging.info(success_string)
 
-# continue on to figure 3c and 3d
-logging.info('Calculating Lag Times and Growth Rates')
-sims = set(DF_3b['sim'])
-codes = set(DF_3b['code'])
-lags = []
-rates = []
-# loop over codes
-for code in tqdm(codes, desc='Looping over Codes'):
-    # declare storage variables
-    t_lag = np.zeros(len(sims))
-    rate = np.zeros(len(sims))
-    DF = DF_3b.loc[DF_3b['code'] == code]
-    for i, sim in enumerate(tqdm(sims, desc='Looping over Sims')):
-        # extract data for this sim
-        data = DF.loc[DF['sim'] == sim]
-        t = data['time'].values
-        f = data['fitness'].values
-        # smooth with gaussian filter
-        gaussian_filter = gaussian(30, 10)
-        filtered_signal = convolve1d(f, gaussian_filter/gaussian_filter.sum())
-        # calculate first derivative
-        delt = np.diff(t)
-        t_avg = (t[1:]+t[:-1])/2
-        filt_grad = np.diff(filtered_signal)/delt
-        # find peaks
-        peak_ind = peakutils.indexes(filt_grad, thres=0.05, min_dist=int(30/delt.mean()))
-        # get timestamp for this point
-        t_lag[i] = t_avg[peak_ind[0]]
-        t_ind = int(peak_ind[0])
-        # get estimate for evolutionary rate
-        dt = t[-1]  - t[t_ind]
-        dx = f[-1] - f[t_ind]
-        rate[i] = dx/dt
-    # store arrays in list
-    lags.append(t_lag)
-    rates.append(rate)
-
-# collate data into a dataframe
-logging.info('Collating Data into Dataframe')
-dfs = []
-for (lag, rate, code) in zip(lags, rates, codes):
-    d = pd.DataFrame({
-        'lag' : lag,
-        'rate' : rate,
-        'code' : [code for i in range(len(lag))]
-    })
-    dfs.append(d)
-DF_3cd = pd.concat(dfs)
-# plot 3c and save
-logging.info("Plotting 3C: Lag Time Distributions")
-# plot violin plots for lag times
+# move on to 3C:
+logging.info("Plotting 3C: Mean Fitness Traces")
+colordict = {
+    'Standard Code' : 'blue',
+    'Colorado' : 'red',
+    'FF20' : 'green'
+    'FF16' : 'orange',
+    'Reductionist20' : 'purple'
+    'Reductionist15' : 'brown'
+}
 plt.figure()
-ax2 = sns.violinplot(
-    x='lag',
-    y='code',
-    data=DF_3cd,
-    palette=colordict,
-    inner='box',
-    order=wanted_codes
+ax1 = sns.tsplot(
+    data=DF_3c,
+    time='time',
+    value='fitness',
+    unit='sim',
+    condition='code',
+    color=colordict,
+    ci='sd'
 )
-plt.title('Distribution of Lag Times (N=1000)')
-plt.xlabel('Lag Time (in generations)')
-sns.despine(trim=True)
+# format plot
+logging.info("Formatting 3C")
+sns.despine()
+plt.xlim([0, 1000])
+# plt.ylim([0, 1.3])
+plt.legend()
+plt.title('Mean Fitness vs Time (1000 Simulations)')
+plt.xlabel('Time (in generations)')
+plt.ylabel('Mean Fitness')
+
 # save output
-logging.info("Saving Figure 3C to S3")
+logging.info("Saving Figure 3C and inset to S3")
 figure_basename = '3c_vector.svg'
 figure_path = '/home/ubuntu/' + figure_basename
 figure_s3path = s3_path + figure_basename
@@ -195,77 +168,177 @@ success_string = (
 )
 logging.info(success_string)
 
-# plot 3D and save
-logging.info("Plotting 3D: Evolutionary Rate Distributions")
-# plot violin plots for lag times
-plt.figure()
-ax3 = sns.violinplot(
-    x='rate',
-    y='code',
-    data=DF_3cd,
-    palette=colordict,
-    inner='box',
-    order=wanted_codes
+ax1 = sns.tsplot(
+    data=DF_3b.loc[DF_3b['code'] == 'Standard Code'],
+    time='time',
+    value='fitness',
+    unit='sim',
+    condition='code',
+    color=colordict,
+    ci='sd'
 )
-plt.title('Distribution of Evolutionary Rates (N=1000)')
-plt.xlabel(r'Evolutionary Rates (in \frac{1}{gen^2})')
-sns.despine(trim=True)
-
-# save output
-logging.info("Saving Figure 3D to S3")
-figure_basename = '3d_vector.svg'
+figure_basename = '3c_inset_vector.svg'
 figure_path = '/home/ubuntu/' + figure_basename
 figure_s3path = s3_path + figure_basename
 plt.savefig(figure_path)
 with open(figure_path, 'rb') as data:
     s3.upload_fileobj(data, bucketname, figure_s3path)
 success_string = (
-    "Success! Figure saved to {0}:{1}. ".format(bucketname, figure_s3path)
+    "Success! Inset saved to {0}:{1}. ".format(bucketname, figure_s3path)
     + "Check 'https://s3.console.aws.amazon.com/s3/home?region={0}'".format(s3_region)
     + " to see output figure file."
 )
 logging.info(success_string)
 
-# plot 3E and save
-logging.info("Plotting 3E: Endpoint Fitness Distributions")
-# get endpoint fitness from simulations
-endpoints = {}
-for code in tqdm(codes, desc='Looping through codes'):
-    endpoints[code] = []
-    df = DF_3b.loc[DF_3b['code'] == code]
-    for sim in tqdm(sims, desc='Looping through sims'):
-        endpoints[code].append(df.loc[df['sim'] == sim,'fitness'].iloc[-1])
-DF_endtimes = pd.DataFrame.from_dict(endpoints)
-
-# plot distribution
-plt.figure()
-for code in codes:
-    sns.distplot(
-        DF_endtimes[code],
-        kde=True,
-        hist=True,
-        rug=False,
-        norm_hist=True,
-        color=colordict[code],
-        label=code,
-        rug_kws={"alpha" : 0.03}
-    )
-sns.despine(trim=True)
-plt.xlabel('Endpoint Fitness')
-plt.ylabel('Probability')
-plt.legend()
-plt.title('Distribution of Endpoint Fitnesses')
-# save output
-logging.info("Saving Figure 3E to S3")
-figure_basename = '3e_vector.svg'
-figure_path = '/home/ubuntu/' + figure_basename
-figure_s3path = s3_path + figure_basename
-plt.savefig(figure_path)
-with open(figure_path, 'rb') as data:
-    s3.upload_fileobj(data, bucketname, figure_s3path)
-success_string = (
-    "Success! Figure saved to {0}:{1}. ".format(bucketname, figure_s3path)
-    + "Check 'https://s3.console.aws.amazon.com/s3/home?region={0}'".format(s3_region)
-    + " to see output figure file."
-)
-logging.info(success_string)
+# # continue on to figure 3c and 3d
+# logging.info('Calculating Lag Times and Growth Rates')
+# sims = set(DF_3b['sim'])
+# codes = set(DF_3b['code'])
+# lags = []
+# rates = []
+# # loop over codes
+# for code in tqdm(codes, desc='Looping over Codes'):
+#     # declare storage variables
+#     t_lag = np.zeros(len(sims))
+#     rate = np.zeros(len(sims))
+#     DF = DF_3b.loc[DF_3b['code'] == code]
+#     for i, sim in enumerate(tqdm(sims, desc='Looping over Sims')):
+#         # extract data for this sim
+#         data = DF.loc[DF['sim'] == sim]
+#         t = data['time'].values
+#         f = data['fitness'].values
+#         # smooth with gaussian filter
+#         gaussian_filter = gaussian(30, 10)
+#         filtered_signal = convolve1d(f, gaussian_filter/gaussian_filter.sum())
+#         # calculate first derivative
+#         delt = np.diff(t)
+#         t_avg = (t[1:]+t[:-1])/2
+#         filt_grad = np.diff(filtered_signal)/delt
+#         # find peaks
+#         peak_ind = peakutils.indexes(filt_grad, thres=0.05, min_dist=int(30/delt.mean()))
+#         # get timestamp for this point
+#         t_lag[i] = t_avg[peak_ind[0]]
+#         t_ind = int(peak_ind[0])
+#         # get estimate for evolutionary rate
+#         dt = t[-1]  - t[t_ind]
+#         dx = f[-1] - f[t_ind]
+#         rate[i] = dx/dt
+#     # store arrays in list
+#     lags.append(t_lag)
+#     rates.append(rate)
+#
+# # collate data into a dataframe
+# logging.info('Collating Data into Dataframe')
+# dfs = []
+# for (lag, rate, code) in zip(lags, rates, codes):
+#     d = pd.DataFrame({
+#         'lag' : lag,
+#         'rate' : rate,
+#         'code' : [code for i in range(len(lag))]
+#     })
+#     dfs.append(d)
+# DF_3cd = pd.concat(dfs)
+# # plot 3c and save
+# logging.info("Plotting 3C: Lag Time Distributions")
+# # plot violin plots for lag times
+# plt.figure()
+# ax2 = sns.violinplot(
+#     x='lag',
+#     y='code',
+#     data=DF_3cd,
+#     palette=colordict,
+#     inner='box',
+#     order=wanted_codes
+# )
+# plt.title('Distribution of Lag Times (N=1000)')
+# plt.xlabel('Lag Time (in generations)')
+# sns.despine(trim=True)
+# # save output
+# logging.info("Saving Figure 3C to S3")
+# figure_basename = '3c_vector.svg'
+# figure_path = '/home/ubuntu/' + figure_basename
+# figure_s3path = s3_path + figure_basename
+# plt.savefig(figure_path)
+# with open(figure_path, 'rb') as data:
+#     s3.upload_fileobj(data, bucketname, figure_s3path)
+# success_string = (
+#     "Success! Figure saved to {0}:{1}. ".format(bucketname, figure_s3path)
+#     + "Check 'https://s3.console.aws.amazon.com/s3/home?region={0}'".format(s3_region)
+#     + " to see output figure file."
+# )
+# logging.info(success_string)
+#
+# # plot 3D and save
+# logging.info("Plotting 3D: Evolutionary Rate Distributions")
+# # plot violin plots for lag times
+# plt.figure()
+# ax3 = sns.violinplot(
+#     x='rate',
+#     y='code',
+#     data=DF_3cd,
+#     palette=colordict,
+#     inner='box',
+#     order=wanted_codes
+# )
+# plt.title('Distribution of Evolutionary Rates (N=1000)')
+# plt.xlabel(r'Evolutionary Rates (in \frac{1}{gen^2})')
+# sns.despine(trim=True)
+#
+# # save output
+# logging.info("Saving Figure 3D to S3")
+# figure_basename = '3d_vector.svg'
+# figure_path = '/home/ubuntu/' + figure_basename
+# figure_s3path = s3_path + figure_basename
+# plt.savefig(figure_path)
+# with open(figure_path, 'rb') as data:
+#     s3.upload_fileobj(data, bucketname, figure_s3path)
+# success_string = (
+#     "Success! Figure saved to {0}:{1}. ".format(bucketname, figure_s3path)
+#     + "Check 'https://s3.console.aws.amazon.com/s3/home?region={0}'".format(s3_region)
+#     + " to see output figure file."
+# )
+# logging.info(success_string)
+#
+# # plot 3E and save
+# logging.info("Plotting 3E: Endpoint Fitness Distributions")
+# # get endpoint fitness from simulations
+# endpoints = {}
+# for code in tqdm(codes, desc='Looping through codes'):
+#     endpoints[code] = []
+#     df = DF_3b.loc[DF_3b['code'] == code]
+#     for sim in tqdm(sims, desc='Looping through sims'):
+#         endpoints[code].append(df.loc[df['sim'] == sim,'fitness'].iloc[-1])
+# DF_endtimes = pd.DataFrame.from_dict(endpoints)
+#
+# # plot distribution
+# plt.figure()
+# for code in codes:
+#     sns.distplot(
+#         DF_endtimes[code],
+#         kde=True,
+#         hist=True,
+#         rug=False,
+#         norm_hist=True,
+#         color=colordict[code],
+#         label=code,
+#         rug_kws={"alpha" : 0.03}
+#     )
+# sns.despine(trim=True)
+# plt.xlabel('Endpoint Fitness')
+# plt.ylabel('Probability')
+# plt.legend()
+# plt.title('Distribution of Endpoint Fitnesses')
+# # save output
+# logging.info("Saving Figure 3E to S3")
+# figure_basename = '3e_vector.svg'
+# figure_path = '/home/ubuntu/' + figure_basename
+# figure_s3path = s3_path + figure_basename
+# plt.savefig(figure_path)
+# with open(figure_path, 'rb') as data:
+#     s3.upload_fileobj(data, bucketname, figure_s3path)
+# success_string = (
+#     "Success! Figure saved to {0}:{1}. ".format(bucketname, figure_s3path)
+#     + "Check 'https://s3.console.aws.amazon.com/s3/home?region={0}'".format(s3_region)
+#     + " to see output figure file."
+# )
+# logging.info(success_string)
