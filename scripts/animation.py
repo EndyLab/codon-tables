7449dd2ac2c340310d69397e4e9c5685e0b0819f
Jon <jecalles@stanford.edu>
2018-05-15 13:10:43 -0700

    further bugs

--- scripts/animation.py
+++ scripts/animation.py
@@ -131,7 +131,8 @@ fig = plt.gcf()
 figure_basename = 'sim_vid.gif'
 figure_path = '/home/ubuntu/' + figure_basename
 figure_s3path = s3_path + figure_basename
-animation.save(figure_path, writer='imagemagick', dpi=dpi, fps=fps);
+anim = animation.FuncAnimation(fig, framer, frames=tot_frames)
+anim.save(figure_path, writer='imagemagick', dpi=dpi, fps=fps);
 fig = plt.figure()
 
 logging.info("Uploading Movie to S3")
