245c93c97e38b81b63b6219e5609455aaa37c0fe
jecalles <callesjonathan@gmail.com>
2018-07-14 14:25:32 -0700

    generated and fixed new bug

--- scripts/5c_fig.py
+++ scripts/5c_fig.py
@@ -71,6 +71,7 @@ for s3_filename in pbar:
 logging.info("Download Successful!")
 
 # concatenate dataframes into one big dataframe
+logging.info("Unpacking Dataframes")
 dfs = []
 pbar = tqdm(local_filenames)
 for file in pbar:
@@ -81,7 +82,7 @@ logging.info("Concatenating Dataframes")
 DF = pd.concat(dfs, copy=False)
 
 DF.loc[:,'popfrac'] = (DF.loc[:,'popfrac'] == 0)
-DF.loc[:,'N_0'] /= 1e6logging.info("Unpacking Dataframes")
+DF.loc[:,'N_0'] /= 1e6
 N_0 = list(set(DF.loc[DF['code'] == 'Standard']['N_0']))
 N_0.sort()
 num_reps = len(DF.loc[(DF['N_0']== N_0[0]) & (DF['code'] == 'Standard')])
