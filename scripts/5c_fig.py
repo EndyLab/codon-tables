952266575e3c947220d4a8c6dcb363d7bfab0ceb
jecalles <callesjonathan@gmail.com>
2018-08-21 17:52:32 -0700

    doing a debug

--- scripts/5c_fig.py
+++ scripts/5c_fig.py
@@ -101,6 +101,7 @@ N_0.sort()
 num_reps = len(DF.loc[(DF['N_0']== N_0[0]) & (DF['code'] == 'Standard')])
 codes = [code for code in colordict.keys() if code not in ['FF20', 'FF16', 'Colorado', 'FFQUAD']]
 for code in tqdm(codes, desc='codes'):
+    logging.info("Processing Data for {0}".format(code))
     for n_0 in tqdm(N_0, desc='initial conditions'):
         DF.loc[(DF['code'] == code)&(DF['N_0'] == n_0), 'sim'] = np.arange(num_reps)
 
