2be5c042c8e8729edb41e3c0ab010ee80f3a7832
jecalles <jecalles@stanford.edu>
2018-12-20 12:33:39 -0800

    setting up REDX_translator notebook to convert sfGFP to compatible DNA sequence

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -1,5 +1,26 @@
 # define physical RED15 and RED20 tables
-RED15 = {
+red15 = {
     codon:'*' for codon in utils.tripletCodons
 }
-RED15
+red15['UUC'] = 'F'
+red15['CUA'] = 'L'
+red15['AUG'] = 'M'
+red15['GUC'] = 'V'
+red15['UCA'] = 'S'
+red15['CCG'] = 'P'
+red15['ACC'] = 'T'
+red15['GCA'] = 'A'
+red15['UAC'] = 'Y'
+red15['CAG'] = 'Q'
+red15['AAA'] = 'K'
+red15['GAC'] = 'D'
+red15['UGG'] = 'W'
+red15['CGU'] = 'R'
+red15['GGA'] = 'G'
+
+red20 = copy.deepcopy(RED15)
+red20['AUC'] = 'I'
+red20['CAC'] = 'H'
+red20['AAC'] = 'N'
+red20['GAA'] = 'E'
+red20['UGC'] = 'C'
