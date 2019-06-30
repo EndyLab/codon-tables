e16cf190e3f54031887f593fb7fd3642057ba280
Jon <callesjonathan@gmail.com>
2019-06-30 14:59:35 -0700

    continuing hijacking

--- scripts/all_contours_plotter.py
+++ scripts/all_contours_plotter.py
@@ -80,66 +80,69 @@ logging.info("Concatenating Dataframes")
 DF = pd.concat(dfs, copy=False)
 
 # repackage data
-with open('res/lin_contour_concat.pickle', 'wb') as handle:
+concat_file = 'lin_contour_concat.pickle'
+with open('res/'+ concat_file, 'wb') as handle:
     pickle.dump(DF, handle)
-
-# define useful helper functions
-def contain_probability(DF, code):
-    df = DF.loc[DF['code'] == code]
-    N_0 = list(set(df['N_0']))
-    N_0.sort()
-    num_sims = len(df.loc[df['N_0'] == N_0[0]].loc[0])
-    t = df.loc[df['sim'] == 0]['time']
-    contain_probability = np.zeros((len(N_0), len(t)))
-    for i, n_0 in enumerate(tqdm(N_0, desc='Processing Initial Conditions: ', leave=False)):
-        lildf = df.loc[df['N_0'] == n_0]
-        for j in tqdm(range(len(t)), desc='Processing sims: '):
-            weedf = lildf.loc[j]
-            contain_probability[i, j] = sum(weedf['popfrac'] == 0) / num_sims
-
-    return contain_probability, t, N_0
-
-def contour_plotter(DF, code, path, figure_param):
-    # unpack figure parameters
-    labelsize=figure_param['labelsize']
-    width = figure_param['width']
-    height = figure_param['height']
-
-    plt.rc('font', family=figure_param['font'])
-    plt.rc('xtick', labelsize=figure_param['labelsize'])
-    plt.rc('ytick', labelsize=figure_param['labelsize'])
-    plt.rc('axes', labelsize=figure_param['labelsize'])
-    # call contain_probability to get relevant arrays
-    contour, t, N_0 = contain_probability(DF, code)
-    # plot resulting contour
-    X, Y = np.meshgrid(t, np.array(N_0)/1e6)
-    CS = plt.contour(X, Y, contour, 20, cmap=plt.cm.viridis_r, vmin=0, vmax=1)
-    plt.clabel(CS, inline=1, fontsize=10)#, colors='black')
-    # format figure
-    ax = plt.gca()
-    plt.xticks([i*200 for i in range(6)])
-    plt.clim(0,1)
-    plt.title('{0}'.format(code if code != 'Standard' else 'Standard Code'), fontsize=labelsize)
-    plt.xlabel('Time (generations)')
-    plt.ylabel('Invasive Pop. Fraction')
-    sns.despine()
-    fig = plt.gcf()
-    fig.set_size_inches(width, height)
-    plt.savefig(path+'{0}_contour_lines.svg'.format(code))
-# plot contours
-path = '/home/ubuntu/'
-codes = set(DF['code'])
-for code in tqdm(codes, desc='Plotting Codes'):
-    # plot and format
-    logging.info("Plotting Contours: {0}".format(code))
-    plt.figure()
-    contour_plotter(DF, code, path, figure_param)
-    # upload to s3
-    figure_basename = '{0}_contour_lines.svg'.format(code)
-    figure_path = path + figure_basename
-    figure_s3path = s3_path + figure_basename
-    # testing save functionality
-    plt.savefig(figure_path)
-    logging.info("Uploading Plot: {0}".format(code))
-    with open(figure_path, 'rb') as data:
-        s3.upload_fileobj(data, bucketname, figure_s3path)
+logging.info("Uploading concatenated datafile")
+with open('res/'+outfile, 'rb') as data:
+    s3.upload_fileobj(data, bucketname, s3_path+concat_file)
+# # define useful helper functions
+# def contain_probability(DF, code):
+#     df = DF.loc[DF['code'] == code]
+#     N_0 = list(set(df['N_0']))
+#     N_0.sort()
+#     num_sims = len(df.loc[df['N_0'] == N_0[0]].loc[0])
+#     t = df.loc[df['sim'] == 0]['time']
+#     contain_probability = np.zeros((len(N_0), len(t)))
+#     for i, n_0 in enumerate(tqdm(N_0, desc='Processing Initial Conditions: ', leave=False)):
+#         lildf = df.loc[df['N_0'] == n_0]
+#         for j in tqdm(range(len(t)), desc='Processing sims: '):
+#             weedf = lildf.loc[j]
+#             contain_probability[i, j] = sum(weedf['popfrac'] == 0) / num_sims
+#
+#     return contain_probability, t, N_0
+#
+# def contour_plotter(DF, code, path, figure_param):
+#     # unpack figure parameters
+#     labelsize=figure_param['labelsize']
+#     width = figure_param['width']
+#     height = figure_param['height']
+#
+#     plt.rc('font', family=figure_param['font'])
+#     plt.rc('xtick', labelsize=figure_param['labelsize'])
+#     plt.rc('ytick', labelsize=figure_param['labelsize'])
+#     plt.rc('axes', labelsize=figure_param['labelsize'])
+#     # call contain_probability to get relevant arrays
+#     contour, t, N_0 = contain_probability(DF, code)
+#     # plot resulting contour
+#     X, Y = np.meshgrid(t, np.array(N_0)/1e6)
+#     CS = plt.contour(X, Y, contour, 20, cmap=plt.cm.viridis_r, vmin=0, vmax=1)
+#     plt.clabel(CS, inline=1, fontsize=10)#, colors='black')
+#     # format figure
+#     ax = plt.gca()
+#     plt.xticks([i*200 for i in range(6)])
+#     plt.clim(0,1)
+#     plt.title('{0}'.format(code if code != 'Standard' else 'Standard Code'), fontsize=labelsize)
+#     plt.xlabel('Time (generations)')
+#     plt.ylabel('Invasive Pop. Fraction')
+#     sns.despine()
+#     fig = plt.gcf()
+#     fig.set_size_inches(width, height)
+#     plt.savefig(path+'{0}_contour_lines.svg'.format(code))
+# # plot contours
+# path = '/home/ubuntu/'
+# codes = set(DF['code'])
+# for code in tqdm(codes, desc='Plotting Codes'):
+#     # plot and format
+#     logging.info("Plotting Contours: {0}".format(code))
+#     plt.figure()
+#     contour_plotter(DF, code, path, figure_param)
+#     # upload to s3
+#     figure_basename = '{0}_contour_lines.svg'.format(code)
+#     figure_path = path + figure_basename
+#     figure_s3path = s3_path + figure_basename
+#     # testing save functionality
+#     plt.savefig(figure_path)
+#     logging.info("Uploading Plot: {0}".format(code))
+#     with open(figure_path, 'rb') as data:
+#         s3.upload_fileobj(data, bucketname, figure_s3path)
